3aaaa7dbe2b273b5f74fbbc86d79b27cea438cb2
dongwt <834575475@qq.com>
2021-03-27 14:48:53 +0800

    bio test

---
+++ src/main/java/com/dongwt/nio/io/BioTest.java
@@ -0,0 +1,39 @@
+package com.dongwt.nio.io;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BioTest {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket();
+        server.bind(new InetSocketAddress(8080));
+        while (true){
+            final Socket client = server.accept();//阻塞
+            System.out.println("client:"+ client.getInetAddress().getHostAddress());
+            new Thread(new Runnable() {
+                public void run() {
+                    InputStream in = null;
+                    try {
+                        in = client.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        while (true){
+                            String request = reader.readLine();//阻塞
+                            System.out.println("request:"+ request);
+                            OutputStream out = client.getOutputStream();
+                            out.write((request+"\n\n").getBytes());
+                            out.flush();
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+
+
+        }
+    }
+}
